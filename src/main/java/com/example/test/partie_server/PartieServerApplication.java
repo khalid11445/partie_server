c4665ba7fb12e69d7ca72ce04cd1b0a426737025
khalid <khalidbenhamamou@gmail.com>
2023-06-03 09:57:48 +0200

    first commit

---
+++ src/main/java/com/example/test/partie_server/PartieServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.test.partie_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartieServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PartieServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/test/partie_server/PartieServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.test.partie_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartieServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
